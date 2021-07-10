585c1c7455437a9130ee203ef19893e273060b3d
Rakesh D R <rakesh.d.r@aitrade.ai>
2021-07-10 20:20:59 +0530

    Added docker page visit count

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express=require('express')
 
 const app=express()
 
-app.get('/',(req,res)=>res.send('Hello World!'))
+let count = 0;
+
+app.get('/',(req,res)=> {
+    count++
+    res.send(`this page has been visited ${count} times`)
+})
 
 app.listen(5000,()=>console.log('server up on 5000'))
 
